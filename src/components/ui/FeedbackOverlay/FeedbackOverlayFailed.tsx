61cd77f6c017fe8bc87d6a05ee7a9fa30d9ac9dd
AI-NovaNX <antagita70@gmail.com>
2026-02-14 02:07:25 +0700

    Comment out defaultIcon import in FeedbackOverlayFailed
    Comment out the import statement for defaultIcon.

--- src/components/ui/FeedbackOverlay/FeedbackOverlayFailed.tsx
+++ src/components/ui/FeedbackOverlay/FeedbackOverlayFailed.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Button from "../Button/Button";
-import defaultIcon from "/Assets/MessageFailed.svg";
+//import defaultIcon from "/Assets/MessageFailed.svg";
 
 export interface FeedbackOverlayProps {
   open: boolean;
